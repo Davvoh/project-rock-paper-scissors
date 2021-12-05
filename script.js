683857615b07f56d4819962345dbddb770850289
Davvoh <davorp165@gmail.com>
2021-12-05 23:41:02 +0100

    remade

--- script.js
+++ script.js
@@ -1,93 +1,69 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
 function computerPlay() {
-   let computerSelect= Math.floor(Math.random()*3);
-   if (computerSelect===0) {
-      computerSelect="paper";
-      return computerSelect;
-   }
-   else if (computerSelect===1) {
-      computerSelect="rock";
-      return computerSelect;
-   }
-   else if (computerSelect===2) {
-      computerSelect="scissors";
-      return computerSelect;    
+   let array = [];
+   array[0] = "rock";
+   array[1] = "paper";
+   array[2] = "scissors";
+   
+computerResult = array[Math.floor(Math.random()*array.length)];
+return computerResult;
+}
+
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Choose rock, paper or scissors");
+    computerSelection = computerPlay();
+  
+   if (playerSelection === computerSelection) {
+      return "It's a tie!";
+   } else if (playerSelection == "rock" && computerSelection == "scissors") {
+      playerScore++;
+      return "You've won! You picked rock, computer picked scissors!";
+   } else if (playerSelection == "paper" && computerSelection == "rock") {
+      playerScore++;
+      return "You've won! You picked paper, computer picked rock!";
+   } else if (playerSelection == "scissors" && computerSelection == "paper") {
+      playerScore++;
+      return "You've won! You picked scissors, computer picked paper!";
+   } else {
+      computerScore++;
+      return "Well you've lost, that is embarassing...";
    }
 }
 
-function playerPlay() {
-   let playerSelect= prompt("Pick Rock, Paper or Scissors", "");
-   playerSelect= playerSelect.toLowerCase();
-   return playerSelect;
-}
-
-function playRound() {
-   let playerSelect = playerPlay();
-   let computerSelect = computerPlay();
-   let round = computerSelect+playerSelect;
-     let result;
-     alert("Player Selection: "+playerSelect);
-     alert("Computer Selection: "+computerSelect);
-
-     if (round==="paperrock") {
-        result= "Computer wins!";
-        return result;
-     }
-     else if (round==="paperpaper") {
-        result= "It's a tie!";
-        return result;
-     }
-     else if (round==="paperscissors") {
-        result= "Player wins!";
-        return result;
-     }
-     else if (round==="rockpaper") {
-        result= "Player wins!";
-        return result;
-     }
-     else if (round==="rockrock") {
-        result= "It's a tie!";
-        return result;
-     }
-     else if (round==="rockscissors") {
-        result= "Computer Wins!";
-        return result;
-     }
-     else if (round==="scissorsrock") {
-        result= "Player Wins!";
-        return result;
-     }
-     else if (round==="scissorspaper") {
-        result= "Computer Wins!";
-        return result;
-     }
-     else if (round==="scissorsscissors") {
-        result= "It's a tie!";
-        return result;
-     }
-}
 
 function game() {
-   let playerScore = 0;
-   let computerScore = 0;
-   for (let i = 0; i <5; i++) {
-      let counter = playRound();
-      alert(counter);
-      if (counter=="Computer wins!") {
-         computerScore = computerScore+1;
-      }
-      else if (counter=="Player Wins!") {
-         playerScore = playerScore+1;
-      }
+   for (let i = 0; i < 8; i++) {
+      console.log(playRound());
    }
-   if (playerScore < computerScore||computerScore > playerScore) {
-      alert("Final Score! "+ "\nPlayer Score: "+playerScore+ "\nComputer Score: "+computerScore+"\nFinal Winner: Computer")
-   }
-   else if (playerScore > computerScore||computerScore < playerScore) {
-      alert("Final Score! "+ "\nPlayer Score: "+playerScore+ "\nComputer Score: "+computerScore+"\nFinal Winner: Player")
-   }
-   else {
-      alert("Final Score! "+ "\nPlayer Score: "+playerScore+ "\nComputer Score: "+computerScore+"\n Nobody Wins")
+   if (playerScore > computerScore) {
+      console.log("You won the game of 8 rounds!");
+   } else if (playerScore < computerScore) {
+      console.log("You lost the game of 8 rounds");
    }
 }
 
+
+
 game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
